644f799b90dbc4b077b4f940c8643c1fec0b13b1
Tortrixxgd <zamoradani087@gmail.com>
2022-05-05 03:46:42 -0600

    validando filas

---
+++ cuerpo.py
@@ -0,0 +1,64 @@
+"""
+Crear un sudoku 9x9 con las siguientes reglas
+
+1. Crear tablero de 9x9 (hecho)
+2. validar filas (hecho)
+3. validar columnas
+4. validar segmentos de 3x3
+5. añadir mapa con numeros aleatorios
+6. integrar modo grafico
+
+"""
+class Cuerpo():
+    def __init__(self):
+        self.tablero = []
+        self.listaexclusiva = []
+
+    def creartablero(self):
+        self.tablero = [[4,1,0,0,0,0,0,0,0],
+                        [0,0,0,2,0,0,0,0,0],
+                        [2,7,0,0,0,3,3,0,0],
+                        [0,0,0,0,0,0,0,0,0],
+                        [5,0,0,8,0,0,0,0,0],
+                        [0,8,0,0,0,0,0,1,0],
+                        [0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0],]
+
+    def imprimirtablero(self):
+        for filas in self.tablero:
+            print(filas)
+
+    def pruebadeverificacion(self):
+        for filas in self.tablero:
+            validar = self.validarfilas(filas)
+            if validar == True:
+                print("Valido ",filas)
+            else:
+                print("invalido ", filas)
+
+    
+            
+
+    def validarfilas(self,unicafila):
+        for elemento in unicafila:
+            if elemento != 0:
+                if unicafila.count(elemento) > 1:
+                    return False
+        return True
+
+    """def validarcolumnas(self):
+        for columnas in range(0,9):
+            for filas in range(0,9):
+                self.listaexclusiva.append(self.tablero[filas][columnas])
+                self.validarfilas(self.listaexclusiva)
+                
+            print(self.listaexclusiva)
+            self.listaexclusiva.clear()  """  
+                    
+
+s_cuerpo = Cuerpo()
+s_cuerpo.creartablero()
+s_cuerpo.imprimirtablero()
+s_cuerpo.pruebadeverificacion()
+#s_cuerpo.validarcolumnas()
